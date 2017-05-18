2a0c5061ece60650d84f17c4d159e6621083fb44
spkelly <spkelly18@gmail.com>
2017-05-18 09:26:01 -0600

    added routes to front end files

--- server/server.js
+++ server/server.js
@@ -4,12 +4,19 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
 const messages = require('./routes/classifieds');
-app.use('/classifieds',messages);
+app.use('/api/classifieds',messages);
+app.use('/js', app.static(path.join(__dirname, '/../client/js')));
+app.use('/templates', app.static(path.join(__dirname, '/../client/templates')));
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: './client/'});
+});
 
 const port = process.env.PORT || 3000;
 
@@ -20,6 +27,9 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// app.get('*', (req,res) =>{
+//   res.sendFile('index.html')
+// })
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
